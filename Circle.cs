924bad4a3db779deee3c004e8b11299fcb88adcc
Rays89 <aracheal795@gmail.com>
2018-05-06 00:45:25 -0400

    added a file

---
+++ Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+       private double radius;
+       
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+
+        //this one will calculate circumference and give all numbers after decimal point
+        public double CalculateCircumference()
+        {
+
+            double circumference = 2 * (3.14 * Radius);//you call the big radius coz its public
+            return circumference;
+
+        }
+        //this one will calculate circumference and give you only two numbers after the decimal point
+        public string calculateFormattedCircumference()
+        {
+            double circumference = 2 * (3.14 * Radius);
+            return ($"{circumference, 0:N2}");//N2 means tow numbers after decimal places 
+        }
+
+        public double CalculateArea()
+        {
+            double Area = 3.14 * (Radius * Radius);
+            return Area;
+
+
+        }
+            
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static int Numofcircle = 0;
+        static void Main(string[] args)
+        {
+
+            bool repeat = true;
+
+
+            Circle pie = new Circle();//instatiating 
+
+            Console.WriteLine("Welcome to the Circle Tester ");
+
+            while (repeat)
+            {
+            Console.WriteLine("Enter radius:");
+
+            pie.Radius = double.Parse(Console.ReadLine());//coz its a string so you have to parse it
+
+            Console.WriteLine($"The Area is {pie.CalculateArea()}");
+            Console.WriteLine($"The Circumference is {pie.CalculateCircumference()}");
+            Console.WriteLine($"The FormattedCircumference is {pie.calculateFormattedCircumference()}");
+                Numofcircle++;
+
+            while(true)
+                {
+                    Console.WriteLine("(n) do you want to quit or (y) to add another circle?");
+                    string input = Console.ReadLine();
+
+                    if(input == "n")
+
+                    {
+
+                        Console.WriteLine($"you added {Numofcircle} circle, goodbye");
+                        repeat = false;
+                        break;
+                    }
+
+                    else if(input == "y")
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+
+                        Console.WriteLine("Invalid input, please try again later");
+                    }
+
+                }
+
+                                                 
+            }
+        }
+    }
+}
